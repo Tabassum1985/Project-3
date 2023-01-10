71b20aeab28157b277134e97e7ce3870393a4d48
Tabassum1985 <112669805+Tabassum1985@users.noreply.github.com>
2023-01-09 21:41:31 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,15 @@
+create table "Vizualization2" (
+	"Year" int,
+	"City" varchar not null,
+	"Home_Team_Goals" int,
+	"Away_Team_Goals" int,
+	"Total_Goals" int
+);
+
+
+
+create table "Vizualization3" (
+	"Winner" varchar not null,
+	"Year" int not null,
+	"GoalsScored" varchar not null
+)
